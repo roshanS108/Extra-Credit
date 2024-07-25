5843bbba680821bec78ef51d404d541a0bbbc04d
= <roshanshoti100@gmail.com>
2024-07-24 20:13:09 -0400

    adding pair class

---
+++ Pair.java
@@ -0,0 +1,14 @@
+public class Pair {
+	int key; //Student ID
+	String value; //Student name
+	
+	public Pair() {
+		key = -1;
+		value = null;
+	}
+	
+	public Pair(int id, String name) {
+		key = id;
+		value = name;
+	}
+}
